daa8b620952246b560514bc42416b9832d1c5fb7
mdm177 <quyenmoscoso@gmail.com>
2022-10-11 17:44:00 -0500

    added some functions for noteserviceimpl

--- src/main/java/services/NoteServiceImpl.java
+++ src/main/java/services/NoteServiceImpl.java
@@ -1,8 +1,15 @@
 package services;
+
+import com.devmountain.noteApp.dtos.NoteDto;
+import com.devmountain.noteApp.entities.Note;
+import com.devmountain.noteApp.entities.User;
 import com.devmountain.noteApp.repositories.NoteRepository;
 import com.devmountain.noteApp.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Service
 public class NoteServiceImpl {
@@ -13,5 +20,29 @@ public class NoteServiceImpl {
     private NoteRepository noteRepository;
 
     //For adding a note
+    @Transactional
+    public void addNote(NoteDto noteDto, Long userId){
+        Optional<User> userOptional = userRepository.findById(userId);
+        Note note = new Note(noteDto);
+        userOptional.ifPresent(note::setUser);
+        noteRepository.saveAndFlush(note);
+    }
+
+    @Transactional
+    public void deleteNoteById(Long noteId){
+        Optional<Note> noteOptional = noteRepository.findById(noteId);
+        noteOptional.ifPresent(note -> noteRepository.delete(note));
+    }
+
+    @Transactional
+    public void updateNoteById(NoteDto noteDto){
+        Optional<Note> noteOptional= noteRepository.findById(noteDto.getId());
+        noteOptional.ifPresent(note -> {
+            note.setBody(noteDto.getBody());
+            noteRepository.saveAndFlush(note);
+        });
+    }
+
+
 
 }
